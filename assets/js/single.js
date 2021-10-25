33fb12601cdbeadb971491743d66f4de48f9be32
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-25 16:18:41 -0400

    Added pagination warning to bottom of single repo page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 3- issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
 var getRepoIssues = function(repo) {
     console.log(repo);
 
@@ -11,6 +27,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -59,4 +80,4 @@ var displayIssues = function(issues) {
 
 };
 
-getRepoIssues("raealejandrino/codequiz");
+getRepoIssues("facebook/react");
